8249e83f9bc1b40207152e1c976e410b08f75f73
Soyo Solution <xenialaw418@gmail.com>
2014-12-04 18:10:39 +0800

    Update paging_sample.php
    update view for new library.

--- application/views/paging_sample.php
+++ application/views/paging_sample.php
@@ -1,14 +1,36 @@
-<h2>News List</h2>
-<?php
-     if (!empty($results['results'])){
-        foreach($results['results'] as $row) { 
-            echo "<div>".$row->id." ) ".$row->title."</div>";
-        }
-    }
-?>
-<p>Pagination: <?php echo $this->pagination->create_links(); ?></p>   
-<p>Total Number: <?php echo $results['total_num']; ?></p>
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+<title>CodeIgniter Pagination Library - Soyo Solution</title>
+</head>
+<body>
+<div id="container">
+	<h1 style="text-align:center">Page Navigation in PHP</h1>
+	<table width="400" border="1" align="center">
+		<tr>
+			<td width="100" bgcolor="#CCCCCC"><p>ID</p></td>
+			<td width="300" bgcolor="#CCCCCC">Post Date</td>
+            <td width="300" bgcolor="#CCCCCC">Post Title</td>
+		</tr>
+		<h1 style="text-align:center"></h1>
+			<div id="body">
+ 
+		<?php
+		foreach($results as $data) {
+		?>
+		<tr>
+            <td><?php echo $data->id; ?></td>
+			<td><?php echo $data->post_date; ?></td>
+			<td><?php echo $data->post_title; ?></td>
+		</tr>
+		<?php
+		}
+		?>
+	</table>
+	<div style="text-align:center"><?php echo $links; ?></div>
+</div>
+</div>
 <hr />
-<p>Provided by <a href="http://www.soyosolution.com" target="_blank">Soyo Solution</a>, 
-This content is released under the <a href="http://opensource.org/licenses/MIT" target="_blank"> MIT License</a>.</p>
+</body>
+</html>
